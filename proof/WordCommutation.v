f3ff3779baa361c7abb0650c02bca143519c3f1b
Alexis Arcen <alexis.arcen@etu.univ-orleans.fr>
2026-05-19 15:56:00 +0200

    updated WordCommutation.v

--- proof/WordCommutation.v
+++ proof/WordCommutation.v
@@ -2,24 +2,24 @@
 
 Section Words.
 
-Inductive lettre : Type :=
-| a : lettre
-| b : lettre
-| c : lettre
-| d : lettre.
+Inductive letter : Type :=
+| a : letter
+| b : letter
+| c : letter
+| d : letter.
 
-Inductive mot : Type :=
-| epsilon : mot
-| cons :lettre -> mot -> mot.
+Inductive word : Type :=
+| epsilon : word
+| cons :letter -> word -> word.
 
-Fixpoint conc (u v:mot) : mot :=
+Fixpoint conc (u v : word) : word :=
 match u with
 | epsilon => v
 | cons l u => cons l (conc u v)
 end.
 
 Lemma conc_nil_l :
-  forall (u:mot), u = conc u epsilon.
+  forall (u : word), u = conc u epsilon.
 Proof.
   intro. induction u.
   * simpl. reflexivity.
@@ -27,34 +27,34 @@ Proof.
 Qed.
 
 Lemma conc_assoc :
-  forall (u v w:mot), conc u (conc v w) = conc (conc u v) w.
+  forall (u v w : word), conc u (conc v w) = conc (conc u v) w.
 Proof.
   intros. induction u.
   * simpl. reflexivity.
   * simpl. rewrite IHu. reflexivity.
 Qed.
 
-Fixpoint long (u:mot) : nat :=
+Fixpoint long (u : word) : nat :=
 match u with
 | epsilon => 0
 | cons l u => 1 + long u
 end.
 
-Fixpoint puiss (n:nat) (u:mot) : mot :=
+Fixpoint puiss (n : nat) (u : word) : word :=
 match n with
 | 0 => epsilon
 | S n => conc u (puiss n u)
 end.
 
-Theorem long_0 :
-  forall (u:mot), long u = 0 -> u = epsilon.
+Lemma long_0 :
+  forall (u : word), long u = 0 -> u = epsilon.
 Proof.
   intro. induction u.
   * intro. reflexivity.
   * intro. simpl in H. discriminate.
 Qed.
 
-Fixpoint prefixe (k : nat) (u : mot) : mot :=
+Fixpoint prefixe (k : nat) (u : word) : word :=
 match k with
 |0 => epsilon
 |S k' => match u with
@@ -63,16 +63,8 @@ match k with
         end
 end.
 
-Lemma pref_max :
-  forall u : mot, prefixe (long u) u = u.
-Proof.
-  intro. induction u.
-  - simpl. reflexivity.
-  - simpl. rewrite IHu. reflexivity.
-Qed.
-
 Lemma pref_first_word :
-  forall (u v : mot),  prefixe (long u) (conc u v) = u.
+  forall (u v : word),  prefixe (long u) (conc u v) = u.
 Proof.
   intros. induction u.
   - simpl. reflexivity.
@@ -91,42 +83,24 @@ Proof.
   - simpl. rewrite <- plus_n_Sm. rewrite IHm. reflexivity.
 Qed.
 
-Lemma inf_refl :
-  forall n : nat, n <= n.
-Proof.
-  intro n. apply le_n.
-Qed.
-
-Lemma egal_succ :
-  forall n m : nat, S n = S m -> n=m.
-Proof.
-  intros n m. apply eq_add_S.
-Qed.
-
 Lemma int_strict_positive :
   forall n : nat, S n > 0.
 Proof.
   intro n. unfold gt. unfold lt. induction n.
-  - apply inf_refl.
+  - apply le_n.
   - apply le_S in IHn. assumption.
 Qed.
 
 Lemma succ_pos_strict :
   forall n : nat, n < S n.
 Proof.
-  intro n. unfold lt. apply inf_refl.
-Qed.
-
-Lemma inf_succ :
-  forall n m : nat, S n <= S m -> n<=m.
-Proof.
-  apply le_S_n.
+  intro n. unfold lt. apply le_n.
 Qed.
 
 Lemma sup_succ_strict :
   forall m n : nat, S m > S n -> m > n.
 Proof.
-  intros m n H. unfold gt. unfold lt. unfold gt in H. unfold lt in H. apply inf_succ in H. assumption.
+  intros m n H. unfold gt. unfold lt. unfold gt in H. unfold lt in H. apply le_S_n in H. assumption.
 Qed.
 
 Lemma le_disjunction :
@@ -138,7 +112,7 @@ Proof.
     + intro. right. assert (S n > 0). apply int_strict_positive. unfold gt in H0. assumption.
   - intro n. destruct n.
     + intro. inversion H.
-    + intro. specialize (IHm n). apply inf_succ in H. apply IHm in H. case H.
+    + intro. specialize (IHm n). apply le_S_n in H. apply IHm in H. case H.
       * intro. left. apply eq_S. assumption.
       * intro. right. unfold lt. unfold lt in H0. apply le_n_S in H0. assumption.
 Qed.
@@ -164,18 +138,18 @@ End Arithmetics.
 Section Fine_Wilf.
 
 Lemma pref_inside_first :
-  forall (k : nat) (u v : mot), k<=(long u) -> prefixe k (conc u v) = prefixe k u.
+  forall (k : nat) (u v : word), k<=(long u) -> prefixe k (conc u v) = prefixe k u.
 Proof.
   intro k. induction k.
   * intros. simpl. reflexivity.
   * intro u. intro v. induction u.
     ** intros. simpl in H. inversion H.
-    ** intros. simpl in H. apply inf_succ in H. specialize (IHk u v H). 
+    ** intros. simpl in H. apply le_S_n in H. specialize (IHk u v H). 
        simpl. rewrite IHk. reflexivity.
 Qed.
 
 Lemma case_egal11 : 
-  forall (u v w1 w2 : mot), long u = long v -> conc u w1 = conc v w2 -> u = v.
+  forall (u v w1 w2 : word), long u = long v -> conc u w1 = conc v w2 -> u = v.
 Proof.
   intro u. induction u.
   - intros. simpl in H. symmetry in H. apply long_0 in H. symmetry. assumption.
@@ -186,7 +160,7 @@ Proof.
 Qed.
 
 Lemma case_egal1 :
-forall (u v : mot) (l : lettre), long u = long v -> conc u (cons l v) = conc v (cons l u) -> conc u v = conc v u.
+forall (u v : word) (l : letter), long u = long v -> conc u (cons l v) = conc v (cons l u) -> conc u v = conc v u.
 Proof.
   intros. assert (u=v).
   apply case_egal11 with (w1 := cons l v) (w2 := cons l u). assumption. assumption.
@@ -194,13 +168,13 @@ Proof.
 Qed.
 
 Lemma case_egal :
-  forall (u v : mot), (long(u)=long(v) /\ conc u v = conc v u) -> u=v.
+  forall (u v : word), (long(u)=long(v) /\ conc u v = conc v u) -> u=v.
 Proof.
   intro u. induction u.
    - intros. destruct H. simpl in H0. simpl in H. symmetry in H. apply long_0 in H. symmetry. assumption.
    - intros. destruct H. induction v.
      + simpl in H. inversion H.
-     + simpl in H. apply egal_succ in H. simpl in H0. injection H0. intros. symmetry in H2.
+     + simpl in H. apply eq_add_S in H. simpl in H0. injection H0. intros. symmetry in H2.
         subst. assert (long u = long v -> conc u (cons l v) = conc v (cons l u)). 
         * intro. assumption.
         * apply case_egal1 in H2. f_equal. assert (long(u)=long(v) /\ conc u v = conc v u). split. assumption. assumption.
@@ -208,25 +182,25 @@ Proof.
 Qed.
 
 Lemma three_cases :
-  forall u v : mot, long u = long v \/ long u > long v \/ long u < long v.
+  forall u v : word, long u = long v \/ long u > long v \/ long u < long v.
 Proof.
   intros. apply cases_int.
 Qed.
 
 Lemma puiss_1 :
-  forall (u : mot), u = puiss 1 u. 
+  forall (u : word), u = puiss 1 u. 
 Proof.
   intro. simpl. apply conc_nil_l.
 Qed.
 
 Lemma conc_idem : 
-  forall u v v' : mot, v=v' -> conc u v = conc u v'.
+  forall u v v' : word, v=v' -> conc u v = conc u v'.
 Proof.
   intros. subst. reflexivity.
 Qed.
 
 Lemma conc_egal_2 :
-  forall u v v' : mot, conc u v = conc u v' -> v=v'.
+  forall u v v' : word, conc u v = conc u v' -> v=v'.
 Proof.
   intro u. induction u.
   - intros. simpl in H. assumption.
@@ -234,7 +208,7 @@ Proof.
 Qed.
 
 Lemma conc_puiss1 : 
-  forall (n m : nat) (u : mot), conc (puiss n u) (puiss m u) = puiss (n+m) u.
+  forall (n m : nat) (u : word), conc (puiss n u) (puiss m u) = puiss (n+m) u.
 Proof.
   intros. induction n. 
   * simpl. reflexivity.
@@ -242,7 +216,7 @@ Proof.
 Qed.
 
 Lemma conc_puiss :
-  forall (n m : nat) (v x w : mot), 
+  forall (n m : nat) (v x w : word), 
     (v = puiss n w /\ x = puiss m w) -> conc v x = puiss (n+m) w.
 Proof.
   intro n. destruct n.
@@ -251,7 +225,7 @@ Proof.
 Qed.
 
 Lemma empty_or_larger :
-  forall v : mot, v=epsilon \/ long v>0.
+  forall v : word, v=epsilon \/ long v>0.
 Proof.
   intro v. destruct v.
   - left. reflexivity.
@@ -265,8 +239,8 @@ Proof.
 Qed.
 
 Lemma pref_cut :
-  forall v u :mot, long u > long v /\ long v > 0 /\ prefixe (long v) u = v -> 
-    exists u' : mot, long u' < long u /\ u = conc v u'.
+  forall v u :word, long u > long v /\ long v > 0 /\ prefixe (long v) u = v -> 
+    exists u' : word, long u' < long u /\ u = conc v u'.
 Proof.
   intro. induction v.
   - intros. destruct H. destruct H0. inversion H0.
@@ -343,8 +317,8 @@ Proof.
 Qed.
 
 Lemma fine_wilf_aux :
-  forall (k: nat) (u v : mot), ((long u + long v <= k) /\ conc u v = conc v u) -> 
-  exists (w : mot) (n m : nat), u = (puiss m w) /\ v = (puiss n w).
+  forall (k: nat) (u v : word), ((long u + long v <= k) /\ conc u v = conc v u) -> 
+  exists (w : word) (n m : nat), u = (puiss m w) /\ v = (puiss n w).
 Proof.
   intro k. induction k.
   - intros. destruct H. inversion H. exists v , 0, 0. simpl. apply fine_wilf_aux1 in H2. destruct H2.
@@ -376,12 +350,12 @@ Proof.
 Qed.
 
 Lemma fine_wilf :
-  forall (u v : mot), conc u v = conc v u -> 
-  exists (w : mot) (n m : nat), u = (puiss m w) /\ v = (puiss n w).
+  forall (u v : word), conc u v = conc v u -> 
+  exists (w : word) (n m : nat), u = (puiss m w) /\ v = (puiss n w).
 Proof.
   intros. assert (forall k, ((long u + long v <= k) /\ conc u v = conc v u) -> 
-  exists (w : mot) (n m : nat), u = (puiss m w) /\ v = (puiss n w)). intro k. apply fine_wilf_aux.
-  specialize (H0 (long u + long v)). assert (long u + long v <= long u + long v). apply inf_refl.
+  exists (w : word) (n m : nat), u = (puiss m w) /\ v = (puiss n w)). intro k. apply fine_wilf_aux.
+  specialize (H0 (long u + long v)). assert (long u + long v <= long u + long v). apply le_n.
   assert (long u + long v <= long u + long v /\ conc u v = conc v u). split; assumption. apply H0 in H2.
   destruct H2 as [w [n [m H_egalites]]]. exists w, n, m. assumption.
 Qed.
